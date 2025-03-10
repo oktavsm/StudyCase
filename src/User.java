abd048e06dc63a0c6652d615a8707c4a50e49117
Ablebil <hafizhfaiq07@gmail.com>
2025-03-10 15:49:26 +0700

    feat: add class User

---
+++ src/User.java
@@ -0,0 +1,13 @@
+class User {
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+
+    public void showProfile() {
+        System.out.println(id);
+        System.out.println(name);
+        System.out.println(phone);
+        System.out.println(email);
+    }
+}
