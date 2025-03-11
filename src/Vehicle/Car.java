a2b3970231172ae727211675077271d3a8f5c168
Fatih <hakimfatih1156@gmail.com>
2025-03-11 09:38:59 +0700

    feat: add constructor and rate calculation method to Car class

--- src/Vehicle/Car.java
+++ src/Vehicle/Car.java
@@ -1,5 +1,12 @@
 package Vehicle;
 
 public class Car extends Vehicle {
+      public Car(String plateNumber, String color, String brand, int baseRate) {
+         super(plateNumber, color, brand, baseRate);
+      }
+      @Override
+      public int calculateRate(int distance) {
+         return baseRate * distance;
+      }
     
 }
