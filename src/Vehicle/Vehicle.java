affed95debd3192ff21f9afb3fa9fe806525a221
Fatih <hakimfatih1156@gmail.com>
2025-03-11 09:31:57 +0700

    feat: add properties and constructor to Vehicle class

--- src/Vehicle/Vehicle.java
+++ src/Vehicle/Vehicle.java
@@ -1,5 +1,27 @@
 package Vehicle;
 
 public abstract class Vehicle {
-    
+    String plateNumber;
+    String color;
+    String brand;
+    int baseRate;
+   Vehicle(String plateNumber, String color, String brand, int baseRate) {
+        this.plateNumber = plateNumber;
+        this.color = color;
+        this.brand = brand;
+        this.baseRate = baseRate;
+    }
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+    public String getColor() {
+        return color;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public int getBaseRate() {
+        return baseRate;
+    }
+    public abstract int calculateRate(int distance);
 }
